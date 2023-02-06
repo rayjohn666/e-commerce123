764fe13f9cea7fb345d27d2bb453529b88e09ebe
Hayden Dillon-Lloyd <115908599+rayjohn666@users.noreply.github.com>
2023-02-05 18:44:33 -0800

    updated category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,58 +1,79 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  Category.findAll({
-    include: [Product],
-  })
-  // be sure to include its associated Products
-  .then((categories) => res.json(categories))
-  .catch((error) => res.status(500).json(error))
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  Category.findOne({
-    where: {
-      id: req.params.id
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No product." });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  Category.update(
+    {
+      category_name: req.body.category_name,
     },
-    include: [Product],
-  })
-  // be sure to include its associated Products
-  .then((category) => res.json(category))
-  .catch((error) => res.status(400).json(error))
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedBook) => {
+      res.json(updatedBook);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-  Category.create(req.body)
-  .then((category) => res.status(200).json(category))
-  .catch((error) => res.status(400).json(error))
-});
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-  Category.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-  .then((category) => res.status(200).json(category))
-  .catch((error) => res.status(400).json(error))
-});
+    if (!categoryData) {
+      res.status(404).json({ message: "Not found." });
+      return;
+    }
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-  Category.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-  .then((category) => res.status(200).json(category))
-  .catch((error) => res.status(400).json(error))
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

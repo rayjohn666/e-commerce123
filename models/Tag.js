c97756e685116e0c2b323ff0b66054b4e29edd20
Hayden Dillon-Lloyd <115908599+rayjohn666@users.noreply.github.com>
2023-02-05 17:55:05 -0800

    updated product.js

--- models/Tag.js
+++ models/Tag.js
@@ -6,15 +6,25 @@ class Tag extends Model {}
 
 Tag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     // define columns
   },
+  tag_name, {
+    type: DataTypes.STRING
+  },
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
-  }
+  },
 );
 
 module.exports = Tag;
